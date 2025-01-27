3a30185ab03bc548211013927807396091d8f976
yslamartins <claudiaysla101@gmail.com>
2025-01-27 16:36:42 -0300

    fix: ajustes no estilo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import '../../styles/variables.css';
 
 import CategorySection from './home-components/CategorySection';
 
 export default function Home() {
   return (
-    <div className='bg-[#D9D9D9]'>
+    <div style={{ backgroundColor: 'var(--neutral-gray)' }}>
       <CategorySection/>
     </div>
   )
-  
-  
+   
 }
