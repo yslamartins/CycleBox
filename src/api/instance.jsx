81115ab0566f71a398abd2a413c9e08ea7b2df13
Teresa Campos <teresacampos42@gmail.com>
2025-01-28 19:27:20 -0300

    feat: implementação do products home e funcao para consumir a api

--- src/api/instance.jsx
+++ src/api/instance.jsx
@@ -1,7 +1,15 @@
-import axios from 'axios'
- 
+import axios from 'axios';
+
 const instance = axios.create({
     baseURL: "https://679027cc49875e5a1a947d3c.mockapi.io/produtos"
 });
 
-export default instance;
+export const fetchProdutos = async () => {
+    try {
+        const response = await instance.get('/');
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        throw error;
+    }
+};
