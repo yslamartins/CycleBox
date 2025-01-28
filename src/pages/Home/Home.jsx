6d92a107a265f559c04df2bf4f347bd4941cd484
Teresa Campos <teresacampos42@gmail.com>
2025-01-28 19:29:02 -0300

    fix: correçao no category section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-import CategorySection from './home-components/CategorySection';
 import Header from '../../components/Header';
+import CategorySection from './home-components/CategorySection';
+import ProductsHome from './home-components/ProductsHome';
 
 export default function Home() {
   return (
     <div className='bg-[#D9D9D9]'>
       <Header/>
       <CategorySection/>
+      <ProductsHome />
     </div>
   )
   
