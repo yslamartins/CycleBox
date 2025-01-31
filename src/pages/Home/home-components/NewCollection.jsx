7c4a3b128cc58e8f0969bb7fa63438811e8f6ca8
Teresa Campos <teresacampos42@gmail.com>
2025-01-30 23:27:34 -0300

    fix: posicionamento e opacity

--- src/pages/Home/home-components/NewCollection.jsx
+++ src/pages/Home/home-components/NewCollection.jsx
@@ -4,15 +4,15 @@ import Button from "../../../components/Button";
 
 export default function NewCollection() {
   return (
-    <div className="relative w-full h-[400px] flex items-center justify-center bg-gray-200 mb-4 overflow-hidden">
+    <div className="relative w-full pt-[70px] h-[400px] flex items-center justify-center bg-gray-200 mb-4 overflow-hidden">
       <img
         src="https://spcity.com.br/wp-content/uploads/2016/11/brecho_minha_avo_tinha-1300x700.jpg"
         alt="Banner Nova Coleção"
-        className="absolute inset-0 w-full h-full object-cover opacity-12"
+        className="absolute inset-0 w-full h-full object-cover opacity-50"
       />
 
       <div className="relative z-1 text-center p-4">
-        <p className="text-lg text-gray-700">Novos drops toda semana</p>
+        <p className="text-lg text-gray-900">Novos drops toda semana</p>
         <h1 className="text-3xl font-bold text-[var(--secondary-color)] mt-2">Nova Coleção</h1>
         <NavLink to="/produtos">
           <Button text={"Explorar"} className="mt-4 px-12 py-4 text-xl" />
